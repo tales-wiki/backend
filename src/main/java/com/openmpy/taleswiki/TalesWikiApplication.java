80d54aaf99ef75594529ce7b36039258caf6ea99
openmpy <suhwan@kakao.com>
2025-03-30 00:12:43 +0900

    feat: 초기 설정

---
+++ src/main/java/com/openmpy/taleswiki/TalesWikiApplication.java
@@ -0,0 +1,13 @@
+package com.openmpy.taleswiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalesWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/openmpy/taleswiki/TalesWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openmpy.taleswiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalesWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
