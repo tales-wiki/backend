515e975b8b0ebc7a85416968919241b64e629952
openmpy <suhwan@kakao.com>
2025-03-30 18:26:42 +0900

    feat: Spring REST Docs 추가

--- src/test/java/com/openmpy/taleswiki/article/presentation/ArticleControllerTest.java
+++ src/test/java/com/openmpy/taleswiki/article/presentation/ArticleControllerTest.java
@@ -2,6 +2,12 @@ package com.openmpy.taleswiki.article.presentation;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -14,11 +20,13 @@ import com.openmpy.taleswiki.article.presentation.response.ArticleCreateResponse
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+@AutoConfigureRestDocs
 @WebMvcTest(controllers = ArticleController.class)
 class ArticleControllerTest {
 
@@ -47,12 +55,24 @@ class ArticleControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(body)
                 )
-                .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").value("1"))
                 .andExpect(jsonPath("$.title").value("제목"))
                 .andExpect(jsonPath("$.nickname").value("닉네임"))
                 .andExpect(jsonPath("$.category").value("인물"))
-                .andExpect(jsonPath("$.version").value("1"));
+                .andExpect(jsonPath("$.version").value("1"))
+                .andDo(print())
+                .andDo(
+                        document("createArticle",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                requestFields(
+                                        fieldWithPath("title").description("제목"),
+                                        fieldWithPath("nickname").description("작성자"),
+                                        fieldWithPath("category").description("카테고리"),
+                                        fieldWithPath("content").description("내용")
+                                )
+                        )
+                );
     }
 }
