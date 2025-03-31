3d84990b991507898335e29011eeb4d2b6e02247
openmpy <suhwan@kakao.com>
2025-03-31 15:20:02 +0900

    feat: 아이피가 유효한지 확인하는 기능 추가

--- src/main/java/com/openmpy/taleswiki/common/util/IpAddressUtil.java
+++ src/main/java/com/openmpy/taleswiki/common/util/IpAddressUtil.java
@@ -1,8 +1,16 @@
 package com.openmpy.taleswiki.common.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 public class IpAddressUtil {
+
+    private static final String IP_V4_PATTERN = "^((25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4]\\d|1\\d{2}|[1-9]?\\d)$";
+    private static final String IP_V6_PATTERN = "^(([0-9a-fA-F]{1,4}):){7}([0-9a-fA-F]{1,4})$";
+
+    private IpAddressUtil() {
+        throw new IllegalStateException("유틸리티 클래스입니다.");
+    }
 
     public static String getClientIp(final HttpServletRequest servletRequest) {
         final String[] headers = {
@@ -21,5 +29,13 @@ public class IpAddressUtil {
             }
         }
         return servletRequest.getRemoteAddr();
+    }
+
+    public static boolean isValidIPv4(final String ip) {
+        return Pattern.matches(IP_V4_PATTERN, ip);
+    }
+
+    public static boolean isValidIPv6(final String ip) {
+        return Pattern.matches(IP_V6_PATTERN, ip);
     }
 }
--- src/test/java/com/openmpy/taleswiki/common/util/IpAddressUtilTest.java
+++ src/test/java/com/openmpy/taleswiki/common/util/IpAddressUtilTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 class IpAddressUtilTest {
@@ -34,5 +36,47 @@ class IpAddressUtilTest {
 
         // then
         assertThat(ip).isEqualTo("123.123.123.123");
+    }
+
+    @DisplayName("[통과] 아이피가 IPv4 형식이다.")
+    @Test
+    void ip_address_util_test_03() {
+        // given
+        final String ip = "127.0.0.1";
+
+        // when
+        final boolean isValid = IpAddressUtil.isValidIPv4(ip);
+
+        // then
+        assertThat(isValid).isTrue();
+    }
+
+    @DisplayName("[통과] 아이피가 IPv6 형식이다.")
+    @Test
+    void ip_address_util_test_04() {
+        // given
+        final String ip = "0000:0000:0000:0000:0000:0000:0000:0000";
+
+        // when
+        final boolean isValid = IpAddressUtil.isValidIPv6(ip);
+
+        // then
+        assertThat(isValid).isTrue();
+    }
+
+    @DisplayName("[예외] 아이피가 IPv4 형식이 아니다.")
+    @ParameterizedTest(name = "값: {0}")
+    @ValueSource(strings = {"1", "1.1", "1.1.1", "0000:0000:0000:0000:0000:0000:0000:0000"})
+    void 예외_ip_address_util_test_01(final String value) {
+        // when & then
+        assertThat(IpAddressUtil.isValidIPv4(value)).isFalse();
+    }
+
+    @DisplayName("[예외] 아이피가 IPv6 형식이 아니다.")
+    @ParameterizedTest(name = "값: {0}")
+    @ValueSource(strings = {"1", "1:1", "1:1:1", "127.0.0.1"})
+    void 예외_ip_address_util_test_02(final String value) {
+        // when & then
+        assertThat(IpAddressUtil.isValidIPv6(value)).isFalse();
     }
 }
