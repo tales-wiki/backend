292d34a404ae8e104edfbba117f6a887167e6f9b
openmpy <suhwan@kakao.com>
2025-04-04 16:33:32 +0900

    feat: 로그아웃 기능 추가

--- src/main/java/com/openmpy/taleswiki/member/presentation/MemberController.java
+++ src/main/java/com/openmpy/taleswiki/member/presentation/MemberController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -41,6 +42,12 @@ public class MemberController {
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 
+    @DeleteMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        final ResponseCookie cookie = deleteCookie();
+        return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
+    }
+
     private ResponseCookie createCookie(final String token) {
         return ResponseCookie.from(JwtTokenProvider.ACCESS_TOKEN, token)
                 .httpOnly(cookieProperties.httpOnly())
@@ -49,6 +56,17 @@ public class MemberController {
                 .path(cookieProperties.path())
                 .sameSite(cookieProperties.sameSite())
                 .maxAge(cookieProperties.maxAge())
+                .build();
+    }
+
+    private ResponseCookie deleteCookie() {
+        return ResponseCookie.from(JwtTokenProvider.ACCESS_TOKEN, "")
+                .httpOnly(cookieProperties.httpOnly())
+                .secure(cookieProperties.secure())
+                .domain(cookieProperties.domain())
+                .path(cookieProperties.path())
+                .sameSite(cookieProperties.sameSite())
+                .maxAge(0)
                 .build();
     }
 }
