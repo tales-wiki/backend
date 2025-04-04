e8678fb233219cd1b0d7a91ba56a6ba13932ecb6
openmpy <150704638+openmpy@users.noreply.github.com>
2025-04-04 14:44:37 +0900

    refactor: 게시글 작성자 예외 케이스 추가 (#22)

--- src/main/java/com/openmpy/taleswiki/article/domain/ArticleNickname.java
+++ src/main/java/com/openmpy/taleswiki/article/domain/ArticleNickname.java
@@ -1,5 +1,6 @@
 package com.openmpy.taleswiki.article.domain;
 
+import static com.openmpy.taleswiki.common.exception.CustomErrorCode.INVALID_ARTICLE_NICKNAME_LENGTH;
 import static com.openmpy.taleswiki.common.exception.CustomErrorCode.NOT_ALLOWED_ARTICLE_NICKNAME_NULL_AND_BLANK;
 
 import com.openmpy.taleswiki.common.exception.CustomException;
@@ -13,17 +14,26 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class ArticleNickname {
 
+    private static final int MAX_NICKNAME_LENGTH = 10;
+
     private String value;
 
     public ArticleNickname(final String value) {
         validateBlank(value);
+        validateLength(value);
 
-        this.value = value;
+        this.value = value.trim();
     }
 
     private void validateBlank(final String value) {
         if (value == null || value.isBlank()) {
             throw new CustomException(NOT_ALLOWED_ARTICLE_NICKNAME_NULL_AND_BLANK);
+        }
+    }
+
+    private void validateLength(final String value) {
+        if (value.length() > MAX_NICKNAME_LENGTH) {
+            throw new CustomException(INVALID_ARTICLE_NICKNAME_LENGTH, value.length());
         }
     }
 }
