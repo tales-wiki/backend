39ed1c6bfbe4363f65d8e75f8690285aba23b43e
openmpy <suhwan@kakao.com>
2025-04-05 11:10:49 +0900

    test: 게시글 작성 테스트 케이스 추가

--- src/test/java/com/openmpy/taleswiki/article/application/ArticleServiceTest.java
+++ src/test/java/com/openmpy/taleswiki/article/application/ArticleServiceTest.java
@@ -261,6 +261,23 @@ class ArticleServiceTest {
         assertThat(responses).isEmpty();
     }
 
+    @DisplayName("[통과] 로그인 한 상태에서 게시글을 작성한다.")
+    @Test
+    void article_service_test_12() {
+        // given
+        final ArticleCreateRequest request = new ArticleCreateRequest("제목", "닉네임", "인물", "내용");
+
+        // when
+        final ArticleCreateResponse response =
+                articleService.create(1L, request, Fixture.createMockServetRequest(10));
+
+        // then
+        assertThat(response.title()).isEqualTo("제목");
+        assertThat(response.nickname()).isEqualTo("닉네임");
+        assertThat(response.category()).isEqualTo("인물");
+        assertThat(response.version()).isEqualTo(1);
+    }
+
     @DisplayName("[예외] 해당 카테고리에 이미 작성된 게시글이 존재한다.")
     @Test
     void 예외_article_service_test_01() {
