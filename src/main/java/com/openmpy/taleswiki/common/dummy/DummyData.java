bd7886af044798bd926db6f34595e5d06c534f39
openmpy <suhwan@kakao.com>
2025-04-08 03:22:29 +0900

    feat: 게시글 더미 데이터 추가

--- src/main/java/com/openmpy/taleswiki/common/dummy/DummyData.java
+++ src/main/java/com/openmpy/taleswiki/common/dummy/DummyData.java
@@ -1,5 +1,9 @@
 package com.openmpy.taleswiki.common.dummy;
 
+import com.openmpy.taleswiki.article.domain.Article;
+import com.openmpy.taleswiki.article.domain.ArticleCategory;
+import com.openmpy.taleswiki.article.domain.ArticleVersion;
+import com.openmpy.taleswiki.article.domain.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +16,27 @@ public class DummyData {
 
     @Profile("test")
     @Bean
-    private CommandLineRunner init() {
-        return null;
+    private CommandLineRunner init(
+            final ArticleRepository articleRepository
+    ) {
+        return (args -> saveArticles(articleRepository));
+    }
+
+    private static void saveArticles(final ArticleRepository articleRepository) {
+        for (int i = 0; i < 1000; i++) {
+            ArticleCategory category = ArticleCategory.PERSON;
+
+            if (i % 2 == 0) {
+                category = ArticleCategory.GUILD;
+            }
+
+            final Article article = Article.create("제목" + i, category);
+            final ArticleVersion articleVersion = ArticleVersion.create("작성자" + i, "내용" + i, 10, "127.0.0.1", article);
+
+            article.addVersion(articleVersion);
+            articleRepository.save(article);
+        }
+
+        log.info("생성된 게시글: {}개", articleRepository.count());
     }
 }
