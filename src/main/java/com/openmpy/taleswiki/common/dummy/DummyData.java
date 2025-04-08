bfdcd98ac0b796fffa5d8f1cc0f44e90da4f1c9f
openmpy <suhwan@kakao.com>
2025-04-08 20:35:42 +0900

    refactor: 게시글 더미 데이터 제목 수정

--- src/main/java/com/openmpy/taleswiki/common/dummy/DummyData.java
+++ src/main/java/com/openmpy/taleswiki/common/dummy/DummyData.java
@@ -7,7 +7,9 @@ import com.openmpy.taleswiki.article.domain.repository.ArticleRepository;
 import com.openmpy.taleswiki.member.domain.Member;
 import com.openmpy.taleswiki.member.domain.MemberSocial;
 import com.openmpy.taleswiki.member.domain.repository.MemberRepository;
+import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
@@ -45,6 +47,8 @@ public class DummyData {
     }
 
     private static void saveArticles(final ArticleRepository articleRepository) {
+        final Faker faker = new Faker(new Locale("ko"));
+
         for (int i = 0; i < 1000; i++) {
             ArticleCategory category = ArticleCategory.PERSON;
 
@@ -52,7 +56,8 @@ public class DummyData {
                 category = ArticleCategory.GUILD;
             }
 
-            final Article article = Article.create("제목" + i, category);
+            final String title = faker.name().fullName().replace(" ", "");
+            final Article article = Article.create(title + i, category);
             final ArticleVersion articleVersion = ArticleVersion.create("작성자" + i, "내용" + i, 10, "127.0.0.1", article);
 
             article.addVersion(articleVersion);
