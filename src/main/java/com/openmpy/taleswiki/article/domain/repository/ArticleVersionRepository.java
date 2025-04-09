743bff425186e6d0b2c9f08fb543c3bcf5065d39
openmpy <suhwan@kakao.com>
2025-04-10 03:55:38 +0900

    fix: 어드민 / 게시글 버전 목록 조회 N+1 문제 수정

--- src/main/java/com/openmpy/taleswiki/article/domain/repository/ArticleVersionRepository.java
+++ src/main/java/com/openmpy/taleswiki/article/domain/repository/ArticleVersionRepository.java
@@ -2,6 +2,8 @@ package com.openmpy.taleswiki.article.domain.repository;
 
 import com.openmpy.taleswiki.article.domain.ArticleVersion;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +13,9 @@ public interface ArticleVersionRepository extends JpaRepository<ArticleVersion, 
     @Query("SELECT av FROM ArticleVersion av JOIN FETCH av.article WHERE av.id = :id")
     Optional<ArticleVersion> findByIdWithArticle(@Param("id") final Long id);
 
+    @Query(
+            value = "SELECT av FROM ArticleVersion av JOIN FETCH av.article",
+            countQuery = "SELECT COUNT(av) FROM ArticleVersion av"
+    )
+    Page<ArticleVersion> findAllWithArticle(final PageRequest pageRequest);
 }
