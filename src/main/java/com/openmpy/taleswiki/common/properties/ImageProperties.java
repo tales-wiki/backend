5c0e63887f7221361eacc4966a9b90a0b385feed
openmpy <suhwan@kakao.com>
2025-04-11 18:44:37 +0900

    chore: 필요 없는 파일 제거

--- src/main/java/com/openmpy/taleswiki/common/properties/ImageProperties.java
+++
@@ -1,9 +0,0 @@
-package com.openmpy.taleswiki.common.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties("image.upload")
-public record ImageProperties(
-        String dir
-) {
-}
