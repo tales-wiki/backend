278ee87cfa11f49433119a5a1e27bf976dff152f
openmpy <suhwan@kakao.com>
2025-04-11 18:20:50 +0900

    feat: FileLoaderUtil 기능 추가

--- src/main/java/com/openmpy/taleswiki/common/util/FileLoaderUtil.java
+++ src/main/java/com/openmpy/taleswiki/common/util/FileLoaderUtil.java
@@ -1,9 +1,13 @@
 package com.openmpy.taleswiki.common.util;
 
+import static com.openmpy.taleswiki.common.exception.CustomErrorCode.NOT_FOUND_FILE;
+
+import com.openmpy.taleswiki.common.exception.CustomException;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
+import org.springframework.web.multipart.MultipartFile;
 
 public class FileLoaderUtil {
 
@@ -18,14 +22,24 @@ public class FileLoaderUtil {
 
         try (final InputStream inputStream = classLoader.getResourceAsStream(MARK_DOWN_PATH + fileName)) {
             if (inputStream == null) {
-                throw new IllegalArgumentException("File not found: " + fileName);
+                throw new CustomException(NOT_FOUND_FILE, fileName);
             }
+
             try (final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
                 return reader.lines().collect(Collectors.joining("\n"));
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        } catch (final CustomException e) {
+            throw e;
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
         }
+    }
+
+    public static String getExtension(final MultipartFile file) {
+        final String filename = file.getOriginalFilename();
+        if (filename == null || !filename.contains(".")) {
+            return "";
+        }
+        return filename.substring(filename.lastIndexOf(".") + 1);
     }
 }
